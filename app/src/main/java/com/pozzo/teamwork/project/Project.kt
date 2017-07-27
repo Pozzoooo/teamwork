6a687713adda7a48ffa5f890644c05e8d44ae314
Luiz Gustavo Pozzo <luiz.pozzo@gmail.com>
2017-07-27 18:46:47 +0100

    Kotlin configured

---
+++ app/src/main/java/com/pozzo/teamwork/project/Project.kt
@@ -0,0 +1,6 @@
+package com.pozzo.teamwork.project
+
+/**
+ * @since 27/07/17.
+ */
+data class Project(val id: String)
---
+++ app/src/main/java/com/pozzo/teamwork/user/User.kt
@@ -0,0 +1,6 @@
+package com.pozzo.teamwork.user
+
+/**
+ * @since 27/07/17.
+ */
+data class User(val username: String, val password: String)
